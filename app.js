31f7e2afd2ba5fe80c4fb904b50d17533a82ed37
PopeJuliusII <edgarb@tuta.io>
2020-06-20 13:45:49 +0200

    added animation of floorplan on hover of learn more button and added meta tags to all pages.

--- app.js
+++ app.js
@@ -2,3 +2,18 @@
 // styling on image! (index.html)
 
 // TODO: Slide view on click for About page information?
+
+var element = document.getElementById('btn');
+var image = document.getElementsByClassName('floor');
+
+element.addEventListener('mouseenter', function() {
+  for (let i = 0; i < image.length; i++){
+      image[i].classList.add('trans-' + i)
+  }
+});
+
+element.addEventListener('mouseleave', function() {
+    for (let i = 0; i < image.length; i++){
+        image[i].classList.remove('trans-' + i)
+    }
+});
